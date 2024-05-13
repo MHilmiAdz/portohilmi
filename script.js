169c1e86a4a16df4af7fb08542994e2193231aa2
MHilmiAdz <103296966+MHilmiAdz@users.noreply.github.com>
2024-05-13 15:00:40 +0700

    DESTROY ALL

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var colorChangeButton = document.getElementById("colorChangeButton");
-  
-    colorChangeButton.addEventListener("click", function() {
-      var colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"];
-      var randomColor = colors[Math.floor(Math.random() * colors.length)];
-      document.body.style.backgroundColor = randomColor;
-    });
-  });
